b29af3e86359ceb679a94140fb35052f9069a574
NightShiftKing <98564476+NightShiftKing@users.noreply.github.com>
2024-09-17 15:31:05 -0600

    Add files via upload

--- Cookie Clicker/cookieclicker.py
+++ Cookie Clicker/cookieclicker.py
@@ -8,32 +8,39 @@ pygame.init()
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("cookie clicker")
 
-#game variables
-doExit = False #variable to quit out of game loop
+
 numClicks = 0 
 xpos = 400 
 ypos = 400
 mousePos = (xpos, ypos) 
 
+clock = pygame.time.Clock()
+
 circX = 400
 circY = 400
 radius = 100
 
-CookiePic = pygame.image.load("cookie.jfif")
+isBig = False
+
+CookiePic = pygame.image.load("cookie.jpg")
 CookieRect = CookiePic.get_rect(center=(400,400))
 
+CookiePic2 = pygame.image.load("cookie2.jpg")
+CookieRect2 = CookiePic2.get_rect(center=(400,400))
 
-
-font = pygame.font.Font('calibri', 32)
+font = pygame.font.SysFont('calibri', 32)
 text1 = font.render('score:', False, (0, 200, 200))
 text2 = font.render(str(int(numClicks)), 1, (0, 200, 200))
 
 
 
 def Cookie():
+    CookiePic = pygame.image.load("cookie.jpg")
+    CookieRect = CookiePic.get_rect(center=(400,400))
 
-    CookieColor = (150, 75, 0)
-    pygame.draw.circle(screen, CookieColor, (xpos, ypos), radius)
+    CookiePic2 = pygame.image.load("cookie2.jpg")
+    CookieRect2 = CookiePic2.get_rect(topleft=(390,390))
+
     
 
 
@@ -41,30 +48,47 @@ def Cookie():
 
 
 #BEGIN GAME LOOP######################################################
-while not doExit:
+while True:
+
+    event = pygame.event.wait()
+
+    if event.type == pygame.QUIT:
+        break
+
+    clock.tick(15)
    
    
-    #pygame's way of listening for events (key presses, mouse clicks, etc)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-           doExit = True #lets you quit program
+
 
     #keyboard input-----------------------------------
     
-    if event.type == pygame.MOUSEBUTTONDOWN:
+    if event.type == pygame.MOUSEMOTION:
         mousePos = event.pos
         if math.sqrt((mousePos[0] - circX)**2 + (mousePos[1] - circY)**2)<100: 
-            numClicks+=3
-            print(numClicks)  
-     
+            isBig = True
+        else:
+            isBig = False
+        
+    if event.type == pygame.MOUSEBUTTONDOWN :
+        if math.sqrt((mousePos[0] - circX)**2 + (mousePos[1] - circY)**2)<100: 
+            numClicks+=1
+        
 
         
      
     #render section-----------------------------------vis
-    Cookie()
-    pygame.display.flip()
 
-    screen.blit(CookiePic, CookieRect)
+    screen.fill((0,0,0))
+
+
+  
+    if(isBig == True):
+        screen.blit(CookiePic2, CookieRect2)
+    else: 
+        screen.blit(CookiePic, CookieRect)
+
+
+
     text2 = font.render(str(int(numClicks)), 1, (0, 200, 200))
     screen.blit(text1, (10, 10))
     screen.blit(text2, (110, 10))
